a309d25135827801345717e858c149b863251cd3
Brandon Shega <b.shega@gmail.com>
2020-06-25 21:23:38 -0400

    Removing icons to make Emacs faster

--- init.el
+++ init.el
@@ -13,18 +13,15 @@
        ;;japanese
 
        :completion
-       (company           ; the ultimate code completion backend
-        +childframe)
+       company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy               ; a search engine for love and life
-        +icons
-        +fuzzy)
+       ivy               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+       ;;doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE tags
@@ -63,7 +60,7 @@
        :emacs
        (dired            ; making dired pretty [functional]
        ;;+ranger         ; bringing the goodness of ranger to dired
-        +icons          ; colorful icons for dired-mode
+       ;; +icons          ; colorful icons for dired-mode
         )
        electric          ; smarter, keyword-based electric-indent
        ibuffer           ; interactive buffer management
